398ff4d87a61101448e0b0a232250cd8fff8a177
wrongkindofdoctor <20195932+wrongkindofdoctor@users.noreply.github.com>
2025-05-13 12:23:09 -0400

    update chronyd bash script

--- chronyd/run_playbooks.sh
+++ chronyd/run_playbooks.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
+# set working directory variable to current directory 
+work_dir = $(pwd)
 # Set pyenv env variables 
 set PYENV_ROOT "${HOME}/.pyenv"
 set PATH "${PYENV_ROOT}/bin:${PATH}"
 if command -v pyenv 1>/dev/null 2>&1; then
   eval "$(pyenv init -)"
 fi
-# print simple command trace
+# printcommand trace
 #set -x
 # Activate the ansible pyenv environment
 echo "Actvating ansible environment"
 pyenv activate ansible
 # run the playbook
-ansible-playbook -i orb_inventory.rb --become playbook.yml
+ansible-playbook -i ${work_dir}/../orb_inventory --become playbook.yml
